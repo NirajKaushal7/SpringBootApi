6693d572d5c1f55213fdd6c1fa52b5464c857534
NirajKaushal7 <niraj.kaushal@innogent.in>
2023-09-25 14:44:58 +0530

    Updated

---
+++ src/main/java/com/springboot/Spring_B/SpringBApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Spring_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBApplication.class, args);
+	}
+
+}
